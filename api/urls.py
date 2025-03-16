6ebef18b3e29aa873993d8c23b63737440015609
Shuvo Sarker <Shuvo01797451@gmail.com>
2025-03-16 18:14:21 +0600

    rent request viewset created

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path,include
 from rest_framework_nested import routers
 from advertisement.views import AdvertisementViewSet
-
+from rentRequest.views import RentRequestViewSet
 router = routers.DefaultRouter()
 router.register('advertisements',AdvertisementViewSet,basename='advertisement-list')
+router.register('rent_requests',RentRequestViewSet,basename='rent-list')
 
 
 urlpatterns = [
--- rentRequest/views.py
+++ rentRequest/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from rest_framework.viewsets import ModelViewSet
+from rentRequest.models import RentRequest
+from rentRequest.serializers import RentRequestSerializer
 # Create your views here.
+
+class RentRequestViewSet(ModelViewSet):
+    queryset = RentRequest.objects.all()
+    serializer_class = RentRequestSerializer
