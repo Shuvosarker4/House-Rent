56061443684ca0c383667e84cd0acbd69f59a8a0
Shuvo Sarker <Shuvo01797451@gmail.com>
2025-03-16 18:18:15 +0600

    auth api permissions implemented

--- advertisement/views.py
+++ advertisement/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from advertisement.models import Advertisement
 from advertisement.serializers import AdvertisementSerializer
+from api.permissions import IsAdminOrReadOnly
 # Create your views here.
 
 class AdvertisementViewSet(ModelViewSet):
     queryset = Advertisement.objects.all()
     serializer_class = AdvertisementSerializer
+    permission_classes = [IsAdminOrReadOnly]
 
