df98ad28d9b710b50f293ac7ed05f68a06485371
Shuvo Sarker <Shuvo01797451@gmail.com>
2025-03-26 15:36:39 +0600

    cors headers added

--- house_rent/urls.py
+++ house_rent/urls.py
@@ -26,23 +26,3 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
-
-
-
-1 .   Course Mobile No : 01797451201
-
-2 .   Result of course mobile phone no % 18 :  1
-
-3 .   Unique Name of your project : House Rent Site
-
-4 .   Backend Project Github  Repository  Link: https://github.com/Shuvosarker4/House-Rent
-
-5 .   Backend Project Swagger Documentation Live  Link: https://house-rent-pied-seven.vercel.app/api/v1/
-
-6 .   User Credential
-Admin Credential 
-Email: admin@admin.com
-pass: 123456
-Other user Credential
-Email: test@gmail.com
-pass: TestUser
