35e06499aa043a13b79525e12eeb049144e08fff
Shuvo Sarker <Shuvo01797451@gmail.com>
2025-03-26 15:38:24 +0600

    cors headers added

--- house_rent/settings.py
+++ house_rent/settings.py
@@ -18,7 +18,8 @@ DEBUG = False
 
 CORS_ALLOWED_ORIGINS = [
     "https://house-rent-pied-seven.vercel.app/api/v1",
-    "http://127.0.0.1:8000"
+    "http://127.0.0.1:8000",
+    "http://localhost:5173",
 ]
 ALLOWED_HOSTS = ['.vercel.app','127.0.0.1']
 AUTH_USER_MODEL = 'users.User'
